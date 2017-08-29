682dc03a602f789a568be9970db2d27ba3c19dcf
vdkhoa <vodangkhoa07@gmail.com>
2017-08-30 01:22:02 +0700

    commit source

--- modules/shipping/src/Controller/ShippingController.php
+++ modules/shipping/src/Controller/ShippingController.php
@@ -7,22 +7,32 @@ use Drupal\Core\Database\Database;
 use Drupal\Core\Url;
 
 class ShippingController extends ControllerBase {
- /**
-  *
-  * @return string
-  */
- public function display() {
-  $form = \Drupal::formBuilder()->getForm('\Drupal\shipping\Form\ShippingForm');
-  $build['form'] = $form;
+  /**
+   *
+   * @return string
+   */
+  public function display(){
+    $lstShip = $this->lstShipping();
 
-  return $build;
- }
+    $build['#theme'] = 'shipping_form';
+    $form = \Drupal::formBuilder()->getForm('\Drupal\shipping\Form\ShippingForm');
+    $build['#ship_form'] = $form;
+    $build['#list_ship'] = $lstShip;
+    $build['#attached'] = array(
+        'library' => array(
+            'shipping/shipping.style'
+        )
+    );
 
- public function lstShipping() {
-   $query = db_select('shipping', 'ship');
-   $query->fields('ship');
+    return $build;
+  }
+  public function lstShipping(){
+    $query = db_select('shipping','ship');
+    $query->join('customer', 'cus', 'cus.customer_code = ship.customer_code');
+    $query->fields('ship');
+    $query->fields('cus', ['customer_name']);
 
-   $result = $query->execute()->fetchAll();
+    $result = $query->execute()->fetchAll();
 
    return $result;
  }
